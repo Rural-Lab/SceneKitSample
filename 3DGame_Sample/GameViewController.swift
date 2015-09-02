4aa6ddb7f0babfa0745eced96da006e439e0b371
tjopy13 <tjopy.13@gmail.com>
2015-09-02 13:52:44 +0900

    update

--- 3DGame_Sample/GameViewController.swift
+++ 3DGame_Sample/GameViewController.swift
@@ -4,6 +4,7 @@ import SceneKit
 
 import CoreMotion
 import CoreLocation
+import AVFoundation
 
 class GameViewController: UIViewController ,CLLocationManagerDelegate, SCNSceneRendererDelegate {
     
@@ -14,22 +15,61 @@ class GameViewController: UIViewController ,CLLocationManagerDelegate, SCNSceneR
     // create instance of MotionManager
     let motionManager: CMMotionManager = CMMotionManager()
 
+    // セッション.
+    var mySession : AVCaptureSession!
+    // デバイス.
+    var myDevice : AVCaptureDevice!
+    // 画像のアウトプット.
+    var myImageOutput : AVCaptureStillImageOutput!
+    
+    var degree = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // セッションの作成.
+        mySession = AVCaptureSession()
+        
+        // デバイス一覧の取得.
+        let devices = AVCaptureDevice.devices()
+        
+        // バックカメラをmyDeviceに格納.
+        for device in devices{
+            if(device.position == AVCaptureDevicePosition.Back){
+                myDevice = device as! AVCaptureDevice
+            }
+        }
+        
+        // バックカメラからVideoInputを取得.
+        let videoInput = AVCaptureDeviceInput.deviceInputWithDevice(myDevice, error: nil) as! AVCaptureDeviceInput
+        
+        // セッションに追加.
+        mySession.addInput(videoInput)
+        
+        // 出力先を生成.
+        myImageOutput = AVCaptureStillImageOutput()
+        
+        // セッションに追加.
+        mySession.addOutput(myImageOutput)
+        
+        // 画像を表示するレイヤーを生成.
+        let myVideoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer.layerWithSession(mySession) as! AVCaptureVideoPreviewLayer
+//        myVideoLayer.frame = self.view.bounds
+        myVideoLayer.frame = CGRectMake(0, 0, self.view.frame.width/2, self.view.frame.height/2)
+        myVideoLayer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        myVideoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        // Viewに追加.
+//        self.view.layer.addSublayer(myVideoLayer)
+        
+        // セッション開始.
+        mySession.startRunning()
+        
         
         // シーンオブジェクトを作成。これ以降シーンオブジェクトのルートノードに
         // 子ノードを追加していくことでシーンにオブジェクトを追加していく。
         // ここではdaeファイル(3Dデータ)の読み込みを行っている。
         let scene = SCNScene(named: "art.scnassets/ship.dae")
-        
-        scene!.background.contents = [
-            UIImage(named: "envmap_interstellar/right.tga")!,
-            UIImage(named: "envmap_interstellar/left.tga")!,
-            UIImage(named: "envmap_interstellar/top.tga")!,
-            UIImage(named: "envmap_interstellar/bottom.tga")!,
-            UIImage(named: "envmap_interstellar/front.tga")!,
-            UIImage(named: "envmap_interstellar/back.tga")!
-        ]
         
         // シーンオブジェクトを撮影するためのノードを作成
         let cameraNode = SCNNode()
@@ -67,9 +107,21 @@ class GameViewController: UIViewController ,CLLocationManagerDelegate, SCNSceneR
         let ship = scene!.rootNode.childNodeWithName("ship", recursively: true)!
         // shipに対してアニメーションを設定する。ここではy軸を中心とした永続的な回転を設定している。
         
+//        let aPlaneNode = SCNNode()
+//        // キューブ型の形状（Geometry）を生成
+//        let aPlaneGeometry = SCNPlane(width: self.view.frame.width/32, height: self.view.frame.height/32)
+//        let PlaneMaterial = SCNMaterial()
+////        PlaneMaterial.diffuse.contents = myVideoLayer
+////        // 上記ノードの形状（Geometry）をキューブにする
+////        aPlaneGeometry.materials = [PlaneMaterial]
+//        aPlaneNode.geometry = aPlaneGeometry
+//        
+//        scene!.rootNode.addChildNode(aPlaneNode)
+        
         // シーンを表示するためのビューへの参照を取得
         let scnView = self.view as! SCNView
         
+//        scnView.layer.addSublayer(myVideoLayer)
         // ビューのシーンに今までオブジェクトを追加してきたシーンを代入
         scnView.scene = scene
         
@@ -80,16 +132,7 @@ class GameViewController: UIViewController ,CLLocationManagerDelegate, SCNSceneR
         scnView.showsStatistics = true
         
         // ビューの背景色を黒に指定
-        scnView.backgroundColor = UIColor.blackColor()
-        
-        // ビューがタップされた時のメソッドを指定してリコグナイザを作成し、ビューのジェスチャに追加する
-        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-        let gestureRecognizers = NSMutableArray()
-        gestureRecognizers.addObject(tapGesture)
-        if let existingGestureRecognizers = scnView.gestureRecognizers {
-            gestureRecognizers.addObjectsFromArray(existingGestureRecognizers)
-        }
-        scnView.gestureRecognizers = gestureRecognizers as [AnyObject]
+        scnView.backgroundColor = UIColor.greenColor()
         
         lm = CLLocationManager()
         // 位置情報を取るよう設定
@@ -116,52 +159,12 @@ class GameViewController: UIViewController ,CLLocationManagerDelegate, SCNSceneR
             cameraNode.rotation = SCNQuaternion(x: Float32(quaternion.x), y: Float32(quaternion.y), z: Float32(quaternion.z), w: Float32(quaternion.w))
 
             let gq1 = GLKQuaternionMakeWithAngleAndAxis(GLKMathDegreesToRadians(-90), 1, 0, 0)
-            let gq2 = GLKQuaternionMake(Float(quaternion.x), Float(quaternion.y), Float(quaternion.z), Float(quaternion.w))
+            let gq2 = GLKQuaternionMake(Float(quaternion.x), Float(quaternion.y), Float(self.degree), Float(quaternion.w))
             let qp = GLKQuaternionMultiply(gq1, gq2)
             let rq = SCNVector4Make(qp.x, qp.y, qp.z, qp.w)
             
             cameraNode.orientation = rq
         })
-    }
-    
-    // ビューがタップされた時の挙動を規定する
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
-        
-        // シーンが追加されたビューへの参照を取得
-        let scnView = self.view as! SCNView
-        
-        // ビューのどの位置がタップされたかを読み込む
-        let p = gestureRecognize.locationInView(scnView)
-        // シーンのオブジェクトへのタップ結果をSCNHitTestResultの配列を返すことで取得する
-        let hitResults = scnView.hitTest(p, options: nil)
-        
-        // タップ結果が一つでもあればアニメーション処理を行う
-        if hitResults!.count > 0 {
-            
-            // タップ結果の初めのオブジェクト（箱型オブジェクト）を取り出す
-            let result: AnyObject! = hitResults
-            
-            // 箱型オブジェクトのマテリアルを取り出す
-            let material = result!.node!.geometry!.firstMaterial
-            
-            // 旧来のUIView AnimationAPIの使い方のようにしてマテリアルを赤く染めるアニメーションを設定する
-            SCNTransaction.begin()
-            SCNTransaction.setAnimationDuration(0.5)
-            
-            // アニメーションが終わった時の挙動を指定する
-            SCNTransaction.setCompletionBlock {
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.5)
-                
-                material!.emission.contents = UIColor.blackColor()
-                
-                SCNTransaction.commit()
-            }
-            
-            material!.emission.contents = UIColor.redColor()
-            
-            SCNTransaction.commit()
-        }
     }
     
     override func shouldAutorotate() -> Bool {
@@ -174,6 +177,11 @@ class GameViewController: UIViewController ,CLLocationManagerDelegate, SCNSceneR
         } else {
             return Int(UIInterfaceOrientationMask.All.rawValue)
         }
+    }
+    
+    // コンパスの値を受信
+    func locationManager(manager:CLLocationManager, didUpdateHeading newHeading:CLHeading) {
+        degree = (newHeading.magneticHeading/180)-1.0
     }
     
     override func didReceiveMemoryWarning() {
